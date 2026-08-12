1379519d7969a68af21ea496368f5069ffe7698b
abdul1-art <abdul1125@student.abudzar.sch.id>
2026-08-12 09:54:55 +0700

    youtube

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="min-h-screen bg-[#0f0f0f] text-white">
       <Navbar />
-      <div className="h-[calc(100vh-6rem)] flex overflow-hidden">
+      <div className="flex overflow-hidden">
         <main className="flex-1 overflow-y-auto">
           <div className="sticky top-0 z-10 bg-[#0f0f0f] border-b border-[#303030]">
             <TopMenu />
